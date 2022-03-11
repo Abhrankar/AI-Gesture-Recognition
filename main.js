e0de94676d09c7671e73b04657dc9e870ab536de
Abhrankar <84310569+Abhrankar@users.noreply.github.com>
2022-03-11 12:49:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function preload() {
 
 }
@@ -13,16 +19,30 @@ function setup() {
     poseNet.on('pose', gotPoses);
 }
 
-function draw() {
-    background('#969A97');
-}
-
 function modelLoaded() {
     console.log('PoseNet Is Initialized!');
 }
 
 function gotPoses(results) {
     if (results.length > 0) {
-        console.log(results)
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX" + rightWristX + "difference = " + difference);
     }
+}
+
+function draw() {
+    background('#969A97');
+
+    document.getElementById("square_side").innerHTML = "Width and Height of a Square will be = " + difference + "px";
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, 100);
 }
